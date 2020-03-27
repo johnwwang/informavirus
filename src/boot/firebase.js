0a4dfeceeaaa8dc605b2bd9ba2ba346ff5fca45c
Daniel <schwartzray8@gmail.com>
2020-03-27 16:24:31 -0500

    authorization

---
+++ src/boot/firebase.js
@@ -0,0 +1,24 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/database";
+
+// PUT YOUR OWN FIREBASE CONFIGURATION HERE
+var firebaseConfig = {
+  apiKey: "AIzaSyARopibwAQzw5eeaWIQcH7No4XSpzy0CIA",
+    authDomain: "informavirus-8b544.firebaseapp.com",
+    databaseURL: "https://informavirus-8b544.firebaseio.com",
+    projectId: "informavirus-8b544",
+    storageBucket: "informavirus-8b544.appspot.com",
+    messagingSenderId: "526335992558",
+    appId: "1:526335992558:web:52938f7b53d53e3b1789ff",
+    measurementId: "G-BGEVYYRR65"
+};
+// Initialize Firebase
+let firebaseApp = firebase.initializeApp(firebaseConfig);
+let firebaseAuth = firebaseApp.auth()
+let firebaseDb = firebaseApp.database()
+
+export { firebaseAuth, firebaseDb }

1f934faa317f1967ce46eb62e03fd3733df633af
Daniel <schwartzray8@gmail.com>
2020-03-28 12:03:49 -0500

    firebase

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,24 +1,29 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
 import * as firebase from "firebase/app";
 
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/database";
 
-// PUT YOUR OWN FIREBASE CONFIGURATION HERE
+
+// Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyARopibwAQzw5eeaWIQcH7No4XSpzy0CIA",
-    authDomain: "informavirus-8b544.firebaseapp.com",
-    databaseURL: "https://informavirus-8b544.firebaseio.com",
-    projectId: "informavirus-8b544",
-    storageBucket: "informavirus-8b544.appspot.com",
-    messagingSenderId: "526335992558",
-    appId: "1:526335992558:web:52938f7b53d53e3b1789ff",
-    measurementId: "G-BGEVYYRR65"
+  apiKey: "AIzaSyDAals3B6FJIplAAAqMnhmtpNteF2t78f4",
+  authDomain: "chat-31b5f.firebaseapp.com",
+  databaseURL: "https://chat-31b5f.firebaseio.com",
+  projectId: "chat-31b5f",
+  storageBucket: "chat-31b5f.appspot.com",
+  messagingSenderId: "502415556218",
+  appId: "1:502415556218:web:7d8d02d8169fba009bc5c3",
+  measurementId: "G-VHX9PNEZMR"
 };
 // Initialize Firebase
 let firebaseApp = firebase.initializeApp(firebaseConfig);
-let firebaseAuth = firebaseApp.auth()
-let firebaseDb = firebaseApp.database()
+// firebase.analytics();
+
+
+let firebaseAuth = firebaseApp.auth();
+let firebaseDb = firebaseApp.database();
 
 export { firebaseAuth, firebaseDb }

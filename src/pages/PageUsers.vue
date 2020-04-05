aca624fb3ceddf846a467df3267ab4cdad4bc45d
Daniel <schwartzray8@gmail.com>
2020-04-05 00:04:35 -0500

    gps

--- src/pages/PageUsers.vue
+++ src/pages/PageUsers.vue
@@ -1,13 +1,51 @@
 <template>
   <q-page class="flex">
-  	<h5>Users page</h5>
-  </q-page>
+  	<template>
+			<div>
+    		GPS position of {{ userDetails.name }}: 
+				<br> <strong>Latitude: {{ position.coords.latitude }}</strong>
+				<br> <strong>Longitude: {{ position.coords.longitude }}</strong>
+  		</div>
+  	</template>
+	</q-page>
 </template>
 
 <script>
-	export default {
-	  
-	}
+import { mapState } from 'vuex'
+import { Plugins } from '@capacitor/core'
+const { Geolocation } = Plugins
+
+export default {
+	computed: {
+		...mapState('store', ['userDetails'])
+	},
+	data () {
+    return {
+      position: 'determining...'
+    }
+	},
+	methods: {
+    getCurrentPosition() {
+      Geolocation.getCurrentPosition().then(position => {
+        console.log('Current', position);
+        this.position = position
+      });
+    }
+	},
+	mounted () {
+    this.getCurrentPosition()
+
+    // we start listening
+    this.geoId = Geolocation.watchPosition({}, (position, err) => {
+      console.log('New GPS position')
+      this.position = position
+    })
+  },
+  beforeDestroy () {
+    // we do cleanup
+    Geolocation.clearWatch(this.geoId)
+  }
+}
 </script>
 
 <style>
